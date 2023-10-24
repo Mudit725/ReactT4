99da74003a5d1b112951ab007085667c3808b4cf
Mudit725 <144220546+Mudit725@users.noreply.github.com>
2023-10-24 23:56:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Details from './components/Details';
+
+function App() {
+  return (
+    <div className="App">
+      <Details/>
+    </div>
+  );
+}
+export default App;
